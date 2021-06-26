b6b54659390e46b061f241a43a3e74bd0cbd731b
jdcera4 <jdcera4@gmail.com>
2021-06-26 15:57:38 -0500

    console

--- console.py
+++ console.py
@@ -1,1 +1,23 @@
-Hola
+#!/usr/bin/python3
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    ''' Console '''
+    prompt = "(hbnb) "
+ 
+    def do_EOF(self, line):
+        ''' Exit the console '''
+        print()
+        return True
+
+    def do_quit(self, line):
+        ''' Quit the console '''
+        return True
+
+    def emptyline(self):
+        ''' Do nothing on empty line '''
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
