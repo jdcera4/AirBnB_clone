a3bd4fa0dc65e4383ebcb11d81cffdc4c85f3147
jdcera4 <jdcera4@gmail.com>
2021-06-28 10:11:00 -0500

    test state

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -37,10 +37,55 @@ class Test_Base_Reqeriments(unittest.TestCase):
     # Documentation for Module, class and methods exist
     def test_docmodule(self):
         """Module is documented"""
-        temp = models.state.__doc__
-        self.assertTrue(temp is not None and len(temp) > 0)
+        self.assertIsNotNone(models.state.__doc__)
 
     def test_docclass(self):
         """Class is documented"""
-        temp = State.__doc__
-        self.assertTrue(temp is not None and len(temp) > 0)
+        self.assertIsNotNone(State.__doc__)
+
+
+class Test_State(unittest.TestCase):
+    """ Class to test the state atributtes """
+    @classmethod
+    def setUpClass(cls):
+        """set up for test"""
+        cls.state = State()
+        cls.state.name = "CA"
+
+    @classmethod
+    def teardownClass(cls):
+        """at the end of the test this will tear it down"""
+        del cls.state
+
+    def tearDown(self):
+        """teardown"""
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+     def test_attributes_State(self):
+        """chekcing if State have attributes"""
+        self.assertTrue('id' in self.state.__dict__)
+        self.assertTrue('created_at' in self.state.__dict__)
+        self.assertTrue('updated_at' in self.state.__dict__)
+        self.assertTrue('name' in self.state.__dict__)
+
+    def test_is_subclass_State(self):
+        """test if State is subclass of BaseModel"""
+        self.assertTrue(issubclass(self.state.__class__, BaseModel), True)
+
+    def test_attribute_types_State(self):
+        """test attribute type for State"""
+        self.assertTrue(hasattr(self.state, "name"))
+        self.assertEqual(type(self.state.name), str)
+        self.assertTrue(self.state.name == "")
+
+    def test_save_State(self):
+        """test if the save works"""
+        self.state.save()
+        self.assertNotEqual(self.state.created_at, self.state.updated_at)
+
+    def test_to_dict_State(self):
+        """test if dictionary works"""
+        self.assertEqual('to_dict' in dir(self.state), True)
