b324bacfb805f76695c4bd25127c5f59ffd7f98d
jdcera4 <jdcera4@gmail.com>
2021-06-28 11:42:47 -0500

    class atrubuttes

--- models/place.py
+++ models/place.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 """ """
+from models.city import city
+from models.users import Users
+from models.amenity import Amenity
 
 
 class Place:
-    pass
+    """ Class for Place """
+    user_id = User.id
+    city_id = City.id
+    name = ""
+    description = ""
+    number_rooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = Amenity.id
