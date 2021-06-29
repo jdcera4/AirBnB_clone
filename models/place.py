5ff0e6be0508a39d3c66a3c8c73440f9b80e3faa
jdcera4 <jdcera4@gmail.com>
2021-06-29 14:06:36 -0500

    users = user

--- models/place.py
+++ models/place.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ """
-from models.city import city
-from models.users import Users
+# from models.city import city
+from models.user import User
 from models.amenity import Amenity
 
 
 class Place:
     """ Class for Place """
-    user_id = User.id
-    city_id = City.id
+    # user_id = User.id
+    # city_id = City.id
     name = ""
     description = ""
     number_rooms = 0
@@ -16,4 +16,4 @@ class Place:
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
-    amenity_ids = Amenity.id
+    # amenity_ids = Amenity.id
