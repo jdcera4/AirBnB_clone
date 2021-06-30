a35e6b426da9fe834a5c957e41e41a0dbc662231
Louvani <paula.louvani@gmail.com>
2021-06-30 18:41:55 -0500

    Super init for review

--- models/review.py
+++ models/review.py
@@ -5,4 +5,10 @@ from models.base_model import BaseModel
 
 
 class Review(BaseModel):
-    pass
+    '''class Review'''
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
