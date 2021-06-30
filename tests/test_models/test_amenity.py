b58d73dd165bb1906b6885353f8647a6c592b76c
jdcera4 <jdcera4@gmail.com>
2021-06-30 15:16:09 -0500

    test amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -5,6 +5,7 @@ import os
 import unittest
 import models.amenity
 from models.amenity import Amenity
+from models.base_model import BaseModel
 
 
 class Test_Base_Reqeriments(unittest.TestCase):
@@ -44,3 +45,27 @@ class Test_Base_Reqeriments(unittest.TestCase):
         """Class is documented"""
         temp = Amenity.__doc__
         self.assertTrue(temp is not None and len(temp) > 0)
+
+
+class Test_Amenity(unittest.TestCase):
+    '''Class to test the Amenity class Attributes'''
+    @classmethod
+    def setUpClass(cls):
+        '''Create instance to test the attributes of a class'''
+        cls.amenity = Amenity()
+
+    @classmethod
+    def tearDownClass(cls):
+        '''Remove the istance after run tests'''
+        del cls.amenity
+
+    def test_inheritance_from_BaseModel(self):
+        '''test if User class is a subclas of BaseModel'''
+        self.assertTrue(issubclass(type(self.amenity), BaseModel))
+
+    def test_attributes(self):
+        '''test if de Amenity class have the attributte and if is str'''
+        # Email
+        self.assertTrue(hasattr(self.amenity, "name"))    # Att exists
+        self.assertIsInstance(self.amenity.name, str)     # Att is string
+        self.assertTrue(self.amenity.name == "")          # Att is empty
